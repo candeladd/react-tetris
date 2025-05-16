fd92438a4f006cb0102c71453c2fe491e2121ff0
Andrew Candelaresi <andrewcandelaresi@Andrews-MacBook-Pro.local>
2025-05-16 00:22:25 -0600

    added next block support in grid block

--- src/app/components/GridBoard.tsx
+++ src/app/components/GridBoard.tsx
@@ -2,18 +2,24 @@ import "../globals.css";
 import Square from "./square";
 import styles from "./styles.module.css";
 
-export default function GridBoard() {
+interface gridBoardProps {
+  rows: number ;
+  cols: number ;
+  class: string;
+}
+
+export default function GridBoard(props: gridBoardProps) {
   const boardArray: React.ReactNode[][] = []
-  for(let row =0; row< 18 ; row++){
+  for(let row =0; row< props.rows ; row++){
     const rowArray: React.ReactNode[] = []
-    for(let col=0; col< 10; col++){
+    for(let col=0; col< props.cols; col++){
       rowArray.push(<Square key={`${row}${col}`} color={Math.floor(Math.random() * 8)}/>)
     }
     boardArray.push(rowArray)
   }
   const gridClass = styles['grid-board']
   return (
-    <div className={gridClass}>
+    <div className={props.class}>
       { boardArray}
     </div>
   )
--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import "../globals.css";
 import styles from "./styles.module.css";
 import GridBoard from "./GridBoard";
+
 export default function Page() {
   const classes = styles['color-4']
   console.log('classes ', classes);
   return (
     <div>
     <h1 className={classes}>Tetris React!</h1>
-    <GridBoard />
+    <GridBoard rows={4} cols={4} class={styles['next-block']}/>
+    <div style={{height: "10px"}}></div>
+    <GridBoard rows={18} cols={10} class={styles['grid-board']}/>
     </div>
   )
 }
